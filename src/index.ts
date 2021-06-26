95495fb10b8ef606d2af265cfbf40a118208631e
David Brutoczki <bcz.david99@gmail.com>
2021-06-26 09:59:28 +0200

    fix: fix optional id in constructor

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ class WebRTCSignaling extends EventEmitter {
 	 * @param id peer id, if leave empty automatically generate with uuid package
 	 * @param url signaling websocket server url, if leave empty use the default free server
 	 */
-	constructor(public id: string, url = "ws://167.172.176.143:8000/") {
+	constructor(public id?: string, url = "ws://167.172.176.143:8000/") {
 		super();
 
 		if (!this.id) this.id = uuid();
